5c0b6bce0bc00524f6356a033cd7b9c5bb3ccd17
Francisco Javier Cortez <francisco.cortez01@utrgv.edu>
2024-10-19 12:36:54 -0500

    server.js file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = requrie("express")
 const mongoose = require("mongoose")
+require('dotenv').config();
+
 const app = express();
 
-const PORT = 8000;
-
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecteding '))
